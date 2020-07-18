1314b014f61a4e172495a394dcdd5dc0fa2a1a1b
John Doe <johndoe@example.com>
2020-07-18 14:55:56 +0300

    add Character compinent and getEpisodeName

--- src/API.js
+++ src/API.js
@@ -1,20 +1,37 @@
 import axios from 'axios';
 
-const instance = axios.create({
-    baseURL: 'https://rickandmortyapi.com/api/'
-});
+/*const baseURL = 'https://rickandmortyapi.com/api/';
+export class ApiFetch {
+    getAllCharacters = async () => {
+        const response = await fetch(`${baseURL}character`);
+        const body = await response.json();
+        const result = body.results;
+        return result;
+    }
+}*/
 
 export default class Api {
 
+    instance = axios.create({
+        baseURL: 'https://rickandmortyapi.com/api/'
+    });
+
     getAllCharacters = async () => {
-        const response = await instance.get('character');
+        const response = await this.instance.get('character');
         const result = response.data.results;
         return result;
     }
 
     getCharacter = async (id) => {
-        const response = await instance.get(`character/${id}`);
+        const response = await this.instance.get(`character/${id}`);
         const result = response.data;
         console.log(result);
+    }
+
+    getEpisodeName = async (url) => {
+        this.instance.defaults.baseURL = "";
+        const response = await this.instance.get(url);
+        const result = response.data.name;
+        return result
     }
 }
